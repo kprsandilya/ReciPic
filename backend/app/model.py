db73224e051fb75e4608f68ae5531e23c0b4362b
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2025-04-12 20:37:49 -0400

    created .env

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,16 +1,14 @@
 import requests
+from google import genai
 
-url = "http://128.211.134.20:8000/infer"
+def get_objects():
+    url = "http://128.211.134.20:8000/infer"
 
-image_name = "spinach.jpg"
+    image_name = "spinach.jpg"
 
-files = {'file': open(f"backend/images/{image_name}", "rb")}
+    files = {'file': open(f"backend/images/{image_name}", "rb")}
 
-response = requests.post(url, files=files)
+    response = requests.post(url, files=files)
 
-if response.status_code == 200:
-    with open("result.txt", "wb") as f:
-        f.write(response.content)
-    print("Inference successful. Result saved to result.txt")
-else:
-    print("Error:", response.text)
+    return response.content
+
