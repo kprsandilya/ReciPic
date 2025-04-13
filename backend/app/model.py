0fe01f47b889e7f3bf7dee70db77dc107fd4d93e
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2025-04-12 20:34:37 -0400

    done with flask on server

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,13 +1,16 @@
 import requests
 
 url = "http://128.211.134.20:8000/infer"
-files = {'file': open("backend/images/spinach.jpg", "rb")}
+
+image_name = "spinach.jpg"
+
+files = {'file': open(f"backend/images/{image_name}", "rb")}
 
 response = requests.post(url, files=files)
 
 if response.status_code == 200:
     with open("result.txt", "wb") as f:
         f.write(response.content)
-    print("Inference successful. Result saved to result.jpg.")
+    print("Inference successful. Result saved to result.txt")
 else:
     print("Error:", response.text)
